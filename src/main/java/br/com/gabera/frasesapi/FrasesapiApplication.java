9faa2f1cf39a1a1b4ae975a400b42fceb396ee12
gaberaspbr <flaviogandrade87@gmail.com>
2024-05-11 09:28:40 -0300

    first commit

---
+++ src/main/java/br/com/gabera/frasesapi/FrasesapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabera.frasesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabera/frasesapi/FrasesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabera.frasesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
